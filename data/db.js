cabfa0b9a114d7d1d7920e7581207700c39476b0
zhou <710972821@qq.com>
2019-05-08 18:04:49 +0800

    handel music

---
+++ data/db.js
@@ -0,0 +1,40 @@
+var articles = [
+  {
+    avatar: '/img/me1.jpg',
+    date: '2019-05-06',
+    time:'3天前',
+    author: 'Tom',
+    title: '我是标题1',
+    img: '/img/b1.jpg',
+    desc: '我是描述1',
+    content:'我是详细内容1',
+    star: 30,
+    view: 40,
+    articleId:'0',
+    music: {
+      src: 'http://oxoxtpvtn.bkt.clouddn.com/%E9%87%91%E5%BF%97%E6%96%87%20-%20%E4%B8%BA%E7%88%B1%E7%97%B4%E7%8B%82.mp3',
+      title: '为爱痴狂',
+      coverImgUrl: 'http://oxoxtpvtn.bkt.clouddn.com/%E4%B8%BA%E7%88%B1%E7%97%B4%E7%8B%82.jpg'
+    }
+  },
+  {
+    avatar: '/img/me2.jpg',
+    date: '2019-05-07',
+    time:'4天前',
+    author:'Leo',
+    title: '我是标题2',
+    img: '/img/b3.jpg',
+    desc: '我是描述2',
+    content: '我是详细内容1',
+    star: 40,
+    view: 50,
+    articleId:'1',
+    music: {
+      src: 'http://sc1.111ttt.cn:8282/2018/1/03/13/396131227447.mp3?tflag=1546606800&pin=97bb2268ae26c20fe093fd5b0f04be80',
+      title: '为爱痴狂',
+    }
+  }
+]
+module.exports = {
+  articles:articles
+}
